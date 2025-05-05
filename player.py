178e6398db9d8b1c1bebe93fcf70e18b59ae12ff
livianoer <livia.dullius@edu.pucrs.br>
2025-05-05 11:02:49 -0300

    Update player.py

--- player.py
+++ player.py
@@ -1,9 +1,15 @@
-class Player:
-    def __init__(self, nome, nickname, ranking):
-        self.nome = nome
+#classe que representa o jogador
+class Player: 
+    def __init__(self, nome, nickname, ranking): 
+        #nome real do jogador
+        self.nome = nome 
+        #apelido usado no jogo
         self.nickname = nickname
-        self.ranking = ranking
-        self.pontos = 70
+        #ranking do jogador
+        self.ranking = ranking 
+        #ponto no torneio (todos começam com 70)
+        self.pontos = 70 
+        #dicionário para contar os eventos recebidos ao longo do torneio
         self.estatisticas = {
             'jogada_original': 0,
             'gafe': 0,
